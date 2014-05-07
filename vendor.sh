e23ecdf9df5a869a3b004b407db3351d90430b8d
Andrew Retmanski <andrew@smoothcontract.com>
2014-05-07 16:32:34 +0100

    Include Batman i18n support by default

--- vendor.sh
+++ vendor.sh
@@ -3,4 +3,4 @@ cd ~/Projects/metapack/batman
 cake build
 
 cp ~/Projects/metapack/batman/build/batman.{js,jquery.js} ~/Projects/metapack/batman-rails/vendor/assets/javascripts/batman/
-cp ~/Projects/metapack/batman/build/extras/batman.rails.js ~/Projects/metapack/batman-rails/vendor/assets/javascripts/batman/
+cp ~/Projects/metapack/batman/build/extras/batman.{i18n.js,rails.js} ~/Projects/metapack/batman-rails/vendor/assets/javascripts/batman/
